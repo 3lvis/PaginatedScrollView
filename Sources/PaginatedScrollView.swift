68cd1c36007dfae5cbea7440f2173f42de6787c6
Elvis <elvisnunez@me.com>
2023-06-30 20:45:21 +0200

    Add files

--- Sources/PaginatedScrollView.swift
+++ Sources/PaginatedScrollView.swift
@@ -2,7 +2,7 @@ import UIKit
 
 public protocol PaginatedScrollViewDataSource: AnyObject {
     func numberOfPagesInPaginatedScrollView(_ paginatedScrollView: PaginatedScrollView) -> Int
-    func paginatedScrollView(_ paginatedScrollView: PaginatedScrollView, controllerAtIndex index: Int) -> UIViewController
+    func paginatedScrollView(_ paginatedScrollView: PaginatedScrollView, viewAtIndex index: Int) -> UIView
 }
 
 public protocol PaginatedScrollViewDelegate: AnyObject {
@@ -10,123 +10,83 @@ public protocol PaginatedScrollViewDelegate: AnyObject {
     func paginatedScrollView(_ paginatedScrollView: PaginatedScrollView, didMoveToIndex index: Int)
 }
 
-open class PaginatedScrollView: UIScrollView {
-    open weak var viewDataSource: PaginatedScrollViewDataSource?
-    open weak var viewDelegate: PaginatedScrollViewDelegate?
-    fileprivate unowned var parentController: UIViewController
-    fileprivate var currentPage: Int
-    fileprivate var shoudEvaluatePageChange = false
+public class PaginatedScrollView: UIView {
+    public weak var dataSource: PaginatedScrollViewDataSource?
 
-    public init(frame: CGRect, parentController: UIViewController, initialPage: Int) {
-        self.parentController = parentController
-        currentPage = initialPage
+    lazy var scrollView: UIScrollView = {
+        let scrollView = UIScrollView()
+        scrollView.isPagingEnabled = true
+        scrollView.translatesAutoresizingMaskIntoConstraints = false
+        return scrollView
+    }()
 
+    lazy var contentView: UIView = {
+        let contentView = UIView()
+        contentView.translatesAutoresizingMaskIntoConstraints = false
+        return contentView
+    }()
+
+    override init(frame: CGRect) {
         super.init(frame: frame)
-
-        #if os(iOS)
-            isPagingEnabled = true
-            scrollsToTop = false
-        #endif
-        showsHorizontalScrollIndicator = false
-        showsVerticalScrollIndicator = false
-
-        delegate = self
-        autoresizingMask = [.flexibleWidth, .flexibleHeight]
-        decelerationRate = .fast
-        backgroundColor = .clear
     }
 
-    required public init?(coder aDecoder: NSCoder) {
+    required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
 
-    open func configure() {
+    override public func layoutSubviews() {
         subviews.forEach { view in
             view.removeFromSuperview()
         }
 
-        let numPages = viewDataSource?.numberOfPagesInPaginatedScrollView(self) ?? 0
-        contentSize = CGSize(width: frame.size.width * CGFloat(numPages), height: frame.size.height)
+        addSubview(scrollView)
+        scrollView.addSubview(contentView)
 
-        loadScrollViewWithPage(currentPage - 1)
-        loadScrollViewWithPage(currentPage)
-        loadScrollViewWithPage(currentPage + 1)
-        gotoPage(currentPage, animated: false)
-    }
+        NSLayoutConstraint.activate([
+            scrollView.centerYAnchor.constraint(equalTo: centerYAnchor),
+            scrollView.heightAnchor.constraint(equalTo: heightAnchor),
 
-    fileprivate func loadScrollViewWithPage(_ page: Int) {
-        let numPages = viewDataSource?.numberOfPagesInPaginatedScrollView(self) ?? 0
-        if page >= numPages || page < 0 {
-            return
-        }
+            scrollView.leadingAnchor.constraint(equalTo: leadingAnchor),
+            scrollView.trailingAnchor.constraint(equalTo: trailingAnchor),
 
-        if let controller = viewDataSource?.paginatedScrollView(self, controllerAtIndex: page), controller.view.superview == nil {
-            var frame = self.frame
-            frame.origin.x = frame.size.width * CGFloat(page)
-            frame.origin.y = 0
-            controller.view.frame = frame
+            contentView.centerYAnchor.constraint(equalTo: scrollView.centerYAnchor),
+            contentView.heightAnchor.constraint(equalTo: scrollView.heightAnchor),
+            contentView.leadingAnchor.constraint(equalTo: scrollView.leadingAnchor),
+            contentView.trailingAnchor.constraint(equalTo: scrollView.trailingAnchor)
+        ])
 
-            parentController.addChild(controller)
-            addSubview(controller.view)
-            controller.didMove(toParent: parentController)
-        }
-    }
+        var previousViewElement: UIView!
 
-    public func gotoPage(_ page: Int, animated: Bool) {
-        loadScrollViewWithPage(page - 1)
-        loadScrollViewWithPage(page)
-        loadScrollViewWithPage(page + 1)
-
-        var bounds = self.bounds
-        bounds.origin.x = bounds.size.width * CGFloat(page)
-        bounds.origin.y = 0
-        scrollRectToVisible(bounds, animated: animated)
-    }
-
-    public func goToNextPage(animated: Bool) {
-        let numPages = viewDataSource?.numberOfPagesInPaginatedScrollView(self) ?? 0
-        let newPage = currentPage + 1
-        if newPage < numPages {
-            viewDelegate?.paginatedScrollView(self, willMoveFromIndex: currentPage)
-            gotoPage(currentPage + 1, animated: animated)
-            viewDelegate?.paginatedScrollView(self, didMoveToIndex: newPage)
-            currentPage = newPage
-        }
-    }
-
-    public func goToPreviousPage(animated: Bool) {
-        let newPage = currentPage - 1
-        if newPage >= 0 {
-            viewDelegate?.paginatedScrollView(self, willMoveFromIndex: currentPage)
-            gotoPage(currentPage - 1, animated: animated)
-            viewDelegate?.paginatedScrollView(self, didMoveToIndex: newPage)
-            currentPage = newPage
-        }
-    }
-}
-
-extension PaginatedScrollView: UIScrollViewDelegate {
-    public func scrollViewWillBeginDragging(_ scrollView: UIScrollView) {
-        shoudEvaluatePageChange = true
-    }
-
-    public func scrollViewDidEndDecelerating(_ scrollView: UIScrollView) {
-        shoudEvaluatePageChange = false
-    }
-
-    public func scrollViewDidScroll(_ scrollView: UIScrollView) {
-        if shoudEvaluatePageChange {
-            let pageWidth = frame.size.width
-            let page = Int(floor((contentOffset.x - pageWidth / 2) / pageWidth) + 1)
-            if page != currentPage {
-                viewDelegate?.paginatedScrollView(self, didMoveToIndex: page)
-                viewDelegate?.paginatedScrollView(self, willMoveFromIndex: currentPage)
+        let numberOfSubViews = dataSource?.numberOfPagesInPaginatedScrollView(self) ?? 0
+        for index in 0..<numberOfSubViews {
+            guard let view = dataSource?.paginatedScrollView(self, viewAtIndex: index) else {
+                continue
             }
-            currentPage = page
 
-            loadScrollViewWithPage(page - 1)
-            loadScrollViewWithPage(page)
-            loadScrollViewWithPage(page + 1)
+            contentView.addSubview(view)
+
+            NSLayoutConstraint.activate([
+                view.centerYAnchor.constraint(equalTo: contentView.centerYAnchor),
+                view.heightAnchor.constraint(equalTo: contentView.heightAnchor),
+                view.widthAnchor.constraint(equalTo: scrollView.widthAnchor)
+            ])
+
+            if previousViewElement == nil {
+                NSLayoutConstraint.activate([
+                    view.leadingAnchor.constraint(equalTo: contentView.leadingAnchor)
+                ])
+            } else {
+                NSLayoutConstraint.activate([
+                    view.leadingAnchor.constraint(equalTo: previousViewElement.trailingAnchor)
+                ])
+            }
+
+            previousViewElement = view
         }
+
+        // At this point previousViewElement refers to the last subview, that is the one at the bottom.
+        NSLayoutConstraint.activate([
+            previousViewElement.trailingAnchor.constraint(equalTo: contentView.trailingAnchor)
+        ])
     }
 }
--- iOS-Demo/RootController.swift
+++ iOS-Demo/RootController.swift
@@ -1,51 +1,49 @@
 import UIKit
 
-@available(iOS 16.0, *)
 class RootController: UIViewController {
-    lazy var nextButton: UIButton = {
-        let button = UIButton(type: .system)
-        button.translatesAutoresizingMaskIntoConstraints = false
-        button.setTitle("Next", for: .normal)
-        button.backgroundColor = .black
-        button.setTitleColor(.white, for: .normal)
-        button.titleLabel?.font = UIFont.systemFont(ofSize: 16)
-        button.layer.cornerRadius = 6
-        button.addTarget(self, action: #selector(nextPage), for: .touchUpInside)
-        return button
-    }()
+    //    lazy var nextButton: UIButton = {
+    //        let button = UIButton(type: .system)
+    //        button.translatesAutoresizingMaskIntoConstraints = false
+    //        button.setTitle("Next", for: .normal)
+    //        button.backgroundColor = .black
+    //        button.setTitleColor(.white, for: .normal)
+    //        button.titleLabel?.font = UIFont.systemFont(ofSize: 16)
+    //        button.layer.cornerRadius = 6
+    //        button.addTarget(self, action: #selector(nextPage), for: .touchUpInside)
+    //        return button
+    //    }()
+    //
+    //    lazy var backButton: UIButton = {
+    //        let button = UIButton(type: .system)
+    //        button.translatesAutoresizingMaskIntoConstraints = false
+    //        button.setTitle("Back", for: .normal)
+    //        button.setTitleColor(.black, for: .normal)
+    //        button.titleLabel?.font = UIFont.systemFont(ofSize: 16)
+    //        button.alpha = 0
+    //        button.addTarget(self, action: #selector(previousPage), for: .touchUpInside)
+    //        return button
+    //    }()
 
-    lazy var backButton: UIButton = {
-        let button = UIButton(type: .system)
-        button.translatesAutoresizingMaskIntoConstraints = false
-        button.setTitle("Back", for: .normal)
-        button.setTitleColor(.black, for: .normal)
-        button.titleLabel?.font = UIFont.systemFont(ofSize: 16)
-        button.alpha = 0
-        button.addTarget(self, action: #selector(previousPage), for: .touchUpInside)
-        return button
-    }()
-
-    let clock = ContinuousClock()
-
-    lazy var pages: [UIViewController] = {
-        var pages = [UIViewController]()
+    lazy var pages: [UIView] = {
+        var pages = [UIView]()
 
         let colorsArray = [UIColor.red, UIColor.green, UIColor.blue,
                            UIColor.cyan, UIColor.magenta, UIColor.yellow]
 
-        for _ in 0..<100000 {
-            let controller = UIViewController()
-            controller.view.backgroundColor = colorsArray[Int(arc4random_uniform(UInt32(colorsArray.count)))] as UIColor
-            pages.append(controller)
+        for _ in 0..<10 {
+            let view = UIView()
+            view.translatesAutoresizingMaskIntoConstraints = false
+            view.backgroundColor = colorsArray[Int(arc4random_uniform(UInt32(colorsArray.count)))] as UIColor
+            pages.append(view)
         }
 
         return pages
     }()
 
     lazy var scrollView: PaginatedScrollView = {
-        let view = PaginatedScrollView(frame: self.view.frame, parentController: self, initialPage: 0)
-        view.viewDataSource = self
-        view.viewDelegate = self
+        let view = PaginatedScrollView()
+        view.translatesAutoresizingMaskIntoConstraints = false
+        view.dataSource = self
         return view
     }()
 
@@ -53,70 +51,66 @@ class RootController: UIViewController {
         super.viewDidLoad()
 
         view.addSubview(scrollView)
-
-        view.addSubview(nextButton)
-        view.addSubview(backButton)
-
         NSLayoutConstraint.activate([
-            backButton.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: -20),
-            backButton.widthAnchor.constraint(greaterThanOrEqualToConstant: 234),
-            backButton.heightAnchor.constraint(equalToConstant: 50),
-            backButton.centerXAnchor.constraint(equalTo: view.centerXAnchor),
-
-            nextButton.bottomAnchor.constraint(equalTo: backButton.topAnchor, constant: -16),
-            nextButton.widthAnchor.constraint(greaterThanOrEqualToConstant: 234),
-            nextButton.heightAnchor.constraint(equalToConstant: 50),
-            nextButton.centerXAnchor.constraint(equalTo: view.centerXAnchor)
+            scrollView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            scrollView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            scrollView.topAnchor.constraint(equalTo: view.topAnchor),
+            scrollView.bottomAnchor.constraint(equalTo: view.bottomAnchor)
         ])
+
+        //        view.addSubview(nextButton)
+        //        view.addSubview(backButton)
+        //
+        //        NSLayoutConstraint.activate([
+        //            backButton.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: -20),
+        //            backButton.widthAnchor.constraint(greaterThanOrEqualToConstant: 234),
+        //            backButton.heightAnchor.constraint(equalToConstant: 50),
+        //            backButton.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+        //
+        //            nextButton.bottomAnchor.constraint(equalTo: backButton.topAnchor, constant: -16),
+        //            nextButton.widthAnchor.constraint(greaterThanOrEqualToConstant: 234),
+        //            nextButton.heightAnchor.constraint(equalToConstant: 50),
+        //            nextButton.centerXAnchor.constraint(equalTo: view.centerXAnchor)
+        //        ])
     }
 
-    override func viewWillLayoutSubviews() {
-        super.viewWillLayoutSubviews()
-
-        let result = clock.measure {
-            scrollView.configure()
-        }
-        print(result)
-    }
-
-    @objc func nextPage() {
-        scrollView.goToNextPage(animated: true)
-    }
-
-    @objc func previousPage() {
-        scrollView.goToPreviousPage(animated: true)
-    }
+    //    @objc func nextPage() {
+    //        scrollView.goToNextPage(animated: true)
+    //    }
+    //
+    //    @objc func previousPage() {
+    //        scrollView.goToPreviousPage(animated: true)
+    //    }
 }
 
-@available(iOS 16.0, *)
 extension RootController: PaginatedScrollViewDataSource {
     func numberOfPagesInPaginatedScrollView(_ paginatedScrollView: PaginatedScrollView) -> Int {
         return pages.count
     }
 
-    func paginatedScrollView(_ paginatedScrollView: PaginatedScrollView, controllerAtIndex index: Int) -> UIViewController {
+    func paginatedScrollView(_ paginatedScrollView: PaginatedScrollView, viewAtIndex index: Int) -> UIView {
         return pages[index]
     }
 }
 
-@available(iOS 16.0, *)
-extension RootController: PaginatedScrollViewDelegate {
-    func paginatedScrollView(_ paginatedScrollView: PaginatedScrollView, didMoveToIndex index: Int) {
-        UIView.animate(withDuration: 0.3) {
-            if index == 0 {
-                self.backButton.alpha = 0
-                self.nextButton.alpha = 1
-            } else if index == self.pages.count - 1 {
-                self.backButton.alpha = 1
-                self.nextButton.alpha = 0
-            } else {
-                self.backButton.alpha = 1
-                self.nextButton.alpha = 1
-            }
-        }
-    }
-
-    func paginatedScrollView(_ paginatedScrollView: PaginatedScrollView, willMoveFromIndex index: Int) {
-
-    }
-}
+//@available(iOS 16.0, *)
+//extension RootController: PaginatedScrollViewDelegate {
+//    func paginatedScrollView(_ paginatedScrollView: PaginatedScrollView, didMoveToIndex index: Int) {
+//        UIView.animate(withDuration: 0.3) {
+//            if index == 0 {
+//                self.backButton.alpha = 0
+//                self.nextButton.alpha = 1
+//            } else if index == self.pages.count - 1 {
+//                self.backButton.alpha = 1
+//                self.nextButton.alpha = 0
+//            } else {
+//                self.backButton.alpha = 1
+//                self.nextButton.alpha = 1
+//            }
+//        }
+//    }
+//
+//    func paginatedScrollView(_ paginatedScrollView: PaginatedScrollView, willMoveFromIndex index: Int) {
+//
+//    }
+//}
